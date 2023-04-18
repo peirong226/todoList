a866361fc433d0e34d571ebf3b28176bc0a669c7
peirong226 <131166806+peirong226@users.noreply.github.com>
2023-04-18 22:59:44 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let section = document.querySelector("section");
+let add = document.querySelector("form button");
+add.addEventListener("click", (e) => {
+  //prevent form from being submitted
+  e.preventDefault();
+
+  //get the input value
+  let form = e.target.parentElement;
+  let todoText = form.children[0].value;
+  let todoMonth = form.children[1].value;
+  let todoDate = form.children[2].value;
+
+  //create a todo
+  let todo = document.createElement("div");
+  todo.classList.add("todo");
+  let text = document.createElement("p");
+  text.classList.add("todo-text");
+  text.innerText = todoText;
+  let time = document.createElement("p");
+  time.classList.add("todo-time");
+  time.innerText = todoMonth + "/" + todoDate;
+  todo.appendChild(text);
+  todo.appendChild(time);
+
+  //create green check and red trash can
+  let completeButton = document.createElement("button");
+  completeButton.classList.add("complete");
+  completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  completeButton.addEventListener("click", (e) => {
+    let todoItem = e.target.parentElement;
+    todoItem.classList.toggle("done");
+  });
+
+  let trashButton = document.createElement("button");
+  trashButton.classList.add("trash");
+  trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+  trashButton.addEventListener("click", (e) => {
+    let todoItem = e.target.parentElement;
+    //用animationend這個event去讓他可以做到某一項刪掉後下面會往上遞補
+    todoItem.addEventListener("animationend", () => {
+      todoItem.remove();
+    });
+    todoItem.style.animation = "scaleDown 0.3s forwards";
+  });
+
+  todo.appendChild(completeButton);
+  todo.appendChild(trashButton);
+
+  todo.style.animation = "scaleUp 0.3s forwards";
+
+  form.children[0].value = ""; //clear the text input
+  section.appendChild(todo);
+});
